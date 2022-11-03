51927748326d09fee316762c5fd1e5a48f46ae0b
Phil Hwang <pjhwang@gmail.com>
2022-11-04 08:46:19 +0900

    Update many things

--- Core/Inc/main.h
+++ Core/Inc/main.h
@@ -59,12 +59,22 @@ void Error_Handler(void);
 /* Private defines -----------------------------------------------------------*/
 #define FAN_CTRL_Pin GPIO_PIN_5
 #define FAN_CTRL_GPIO_Port GPIOA
+#define RELAY_EXT_CTRL_Pin GPIO_PIN_0
+#define RELAY_EXT_CTRL_GPIO_Port GPIOB
+#define EXT_CTRL_Pin GPIO_PIN_1
+#define EXT_CTRL_GPIO_Port GPIOB
+#define RELAY_EXT_CTRL2_Pin GPIO_PIN_2
+#define RELAY_EXT_CTRL2_GPIO_Port GPIOB
 #define FRONT_LED3_Pin GPIO_PIN_10
 #define FRONT_LED3_GPIO_Port GPIOB
 #define FRONT_LED4_Pin GPIO_PIN_12
 #define FRONT_LED4_GPIO_Port GPIOB
 #define AUDIO_MUX_A_Pin GPIO_PIN_13
 #define AUDIO_MUX_A_GPIO_Port GPIOB
+#define LTE_UART_TXD_Pin GPIO_PIN_9
+#define LTE_UART_TXD_GPIO_Port GPIOA
+#define LTE_UART_RXD_Pin GPIO_PIN_10
+#define LTE_UART_RXD_GPIO_Port GPIOA
 #define FRONT_LED1_Pin GPIO_PIN_8
 #define FRONT_LED1_GPIO_Port GPIOB
 #define FRONT_LED2_Pin GPIO_PIN_9
--- Core/Src/main.c
+++ Core/Src/main.c
@@ -19,13 +19,17 @@
 /* Includes ------------------------------------------------------------------*/
 #include "main.h"
 #include "cmsis_os.h"
+#include "stm32f4xx_hal_gpio.h"
 
 /* Private includes ----------------------------------------------------------*/
 /* USER CODE BEGIN Includes */
-
+#include <stdint.h>
+#include <stdio.h>
+#include <string.h>
 /* USER CODE END Includes */
 
 /* Private typedef -----------------------------------------------------------*/
+typedef StaticQueue_t osStaticMessageQDef_t;
 /* USER CODE BEGIN PTD */
 
 /* USER CODE END PTD */
@@ -46,11 +50,22 @@ UART_HandleTypeDef huart6;
 osThreadId_t defaultTaskHandle;
 const osThreadAttr_t defaultTask_attributes = {
   .name = "defaultTask",
-  .stack_size = 128 * 4,
+  .stack_size = 256 * 4,
   .priority = (osPriority_t) osPriorityNormal,
 };
+/* Definitions for RxQueue */
+osMessageQueueId_t RxQueueHandle;
+uint8_t RxQueueBuffer[ 64 * sizeof( uint8_t ) ];
+osStaticMessageQDef_t RxQueueControlBlock;
+const osMessageQueueAttr_t RxQueue_attributes = {
+  .name = "RxQueue",
+  .cb_mem = &RxQueueControlBlock,
+  .cb_size = sizeof(RxQueueControlBlock),
+  .mq_mem = &RxQueueBuffer,
+  .mq_size = sizeof(RxQueueBuffer)
+};
 /* USER CODE BEGIN PV */
-
+uint8_t rxbuf[256];
 /* USER CODE END PV */
 
 /* Private function prototypes -----------------------------------------------*/
@@ -115,6 +130,10 @@ int main(void)
   /* USER CODE BEGIN RTOS_TIMERS */
   /* start timers, add new ones, ... */
   /* USER CODE END RTOS_TIMERS */
+
+  /* Create the queue(s) */
+  /* creation of RxQueue */
+  RxQueueHandle = osMessageQueueNew (64, sizeof(uint8_t), &RxQueue_attributes);
 
   /* USER CODE BEGIN RTOS_QUEUES */
   /* add queues, ... */
@@ -238,8 +257,8 @@ static void MX_GPIO_Init(void)
   HAL_GPIO_WritePin(FAN_CTRL_GPIO_Port, FAN_CTRL_Pin, GPIO_PIN_RESET);
 
   /*Configure GPIO pin Output Level */
-  HAL_GPIO_WritePin(GPIOB, FRONT_LED3_Pin|FRONT_LED4_Pin|AUDIO_MUX_A_Pin|FRONT_LED1_Pin
-                          |FRONT_LED2_Pin, GPIO_PIN_RESET);
+  HAL_GPIO_WritePin(GPIOB, RELAY_EXT_CTRL_Pin|FRONT_LED3_Pin|FRONT_LED4_Pin|AUDIO_MUX_A_Pin
+                          |FRONT_LED1_Pin|FRONT_LED2_Pin, GPIO_PIN_RESET);
 
   /*Configure GPIO pin : FAN_CTRL_Pin */
   GPIO_InitStruct.Pin = FAN_CTRL_Pin;
@@ -248,17 +267,23 @@ static void MX_GPIO_Init(void)
   GPIO_InitStruct.Speed = GPIO_SPEED_FREQ_LOW;
   HAL_GPIO_Init(FAN_CTRL_GPIO_Port, &GPIO_InitStruct);
 
-  /*Configure GPIO pins : FRONT_LED3_Pin FRONT_LED4_Pin AUDIO_MUX_A_Pin FRONT_LED1_Pin
-                           FRONT_LED2_Pin */
-  GPIO_InitStruct.Pin = FRONT_LED3_Pin|FRONT_LED4_Pin|AUDIO_MUX_A_Pin|FRONT_LED1_Pin
-                          |FRONT_LED2_Pin;
+  /*Configure GPIO pins : RELAY_EXT_CTRL_Pin FRONT_LED3_Pin FRONT_LED4_Pin AUDIO_MUX_A_Pin
+                           FRONT_LED1_Pin FRONT_LED2_Pin */
+  GPIO_InitStruct.Pin = RELAY_EXT_CTRL_Pin|FRONT_LED3_Pin|FRONT_LED4_Pin|AUDIO_MUX_A_Pin
+                          |FRONT_LED1_Pin|FRONT_LED2_Pin;
   GPIO_InitStruct.Mode = GPIO_MODE_OUTPUT_PP;
   GPIO_InitStruct.Pull = GPIO_NOPULL;
   GPIO_InitStruct.Speed = GPIO_SPEED_FREQ_LOW;
   HAL_GPIO_Init(GPIOB, &GPIO_InitStruct);
 
-  /*Configure GPIO pins : PA9 PA10 */
-  GPIO_InitStruct.Pin = GPIO_PIN_9|GPIO_PIN_10;
+  /*Configure GPIO pins : EXT_CTRL_Pin RELAY_EXT_CTRL2_Pin */
+  GPIO_InitStruct.Pin = EXT_CTRL_Pin|RELAY_EXT_CTRL2_Pin;
+  GPIO_InitStruct.Mode = GPIO_MODE_INPUT;
+  GPIO_InitStruct.Pull = GPIO_NOPULL;
+  HAL_GPIO_Init(GPIOB, &GPIO_InitStruct);
+
+  /*Configure GPIO pins : LTE_UART_TXD_Pin LTE_UART_RXD_Pin */
+  GPIO_InitStruct.Pin = LTE_UART_TXD_Pin|LTE_UART_RXD_Pin;
   GPIO_InitStruct.Mode = GPIO_MODE_AF_PP;
   GPIO_InitStruct.Pull = GPIO_NOPULL;
   GPIO_InitStruct.Speed = GPIO_SPEED_FREQ_VERY_HIGH;
@@ -276,7 +301,22 @@ static void MX_GPIO_Init(void)
 }
 
 /* USER CODE BEGIN 4 */
+static void _select_audio(int is_output)
+{
+    GPIO_PinState state =
+        HAL_GPIO_ReadPin(AUDIO_MUX_A_GPIO_Port, AUDIO_MUX_A_Pin);
 
+    if (is_output != state) {
+        HAL_GPIO_WritePin(AUDIO_MUX_A_GPIO_Port, AUDIO_MUX_A_Pin,
+                          is_output ? GPIO_PIN_SET : GPIO_PIN_RESET);
+    }
+}
+
+void HAL_UART_RxCpltCallback(UART_HandleTypeDef *huart)
+{
+    _select_audio(1);
+    HAL_UART_Receive_IT(huart, rxbuf, 4);
+}
 /* USER CODE END 4 */
 
 /* USER CODE BEGIN Header_StartDefaultTask */
@@ -288,14 +328,19 @@ static void MX_GPIO_Init(void)
 /* USER CODE END Header_StartDefaultTask */
 void StartDefaultTask(void *argument)
 {
-  /* USER CODE BEGIN 5 */
+    /* USER CODE BEGIN 5 */
 
-  HAL_GPIO_WritePin(AUDIO_MUX_A_GPIO_Port, AUDIO_MUX_A_Pin, GPIO_PIN_SET);
+    /* HAL_GPIO_WritePin(AUDIO_MUX_A_GPIO_Port, AUDIO_MUX_A_Pin, GPIO_PIN_SET); */
 
-  /* Infinite loop */
-  for(;;)
-  {
-    osDelay(1);
+    HAL_UART_Receive_IT(&huart6, rxbuf, 4);
+
+    /* Infinite loop */
+    for (;;) {
+	GPIO_PinState state = HAL_GPIO_ReadPin(EXT_CTRL_GPIO_Port, EXT_CTRL_Pin);
+	if (state == GPIO_PIN_RESET) {
+	    _select_audio(0);
+	}
+	osDelay(10);
   }
   /* USER CODE END 5 */
 }
